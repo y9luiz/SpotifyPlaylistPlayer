4ef27be7f99029e05b6b98d411424701146d942b
Luiz <y9luiz@hotmail.com>
2021-12-12 21:38:24 -0300

    now sendind the redirect uri to receive the authorization answer

--- spotifyplaylistplayer.cpp
+++ spotifyplaylistplayer.cpp
@@ -6,7 +6,6 @@ SpotifyPlayListPlayer::SpotifyPlayListPlayer(QWidget *parent)
     , ui(new Ui::SpotifyPlayListPlayer)
 {
     ui->setupUi(this);
-
 }
 
 SpotifyPlayListPlayer::~SpotifyPlayListPlayer()
@@ -18,5 +17,19 @@ SpotifyPlayListPlayer::~SpotifyPlayListPlayer()
 void SpotifyPlayListPlayer::on_pushButtonGrant_clicked()
 {
     spotifyWrapper.grant();
+    QObject::connect(&spotifyWrapper, &SpotifyWrapper::authenticated, [this](){
+           qDebug() << "authenticated";
+           QNetworkReply *reply = spotifyWrapper.me();
+           QObject::connect(reply, &QNetworkReply::finished, [=]() {
+                   reply->deleteLater();
+                   if (reply->error() != QNetworkReply::NoError) {
+                       qDebug()<<"temos um erro";
+                       qDebug() << reply->errorString();
+                       return;
+                   }
+                   QTimer::singleShot(1000, &QCoreApplication::quit);
+           });
+       });
+
 }
 
--- spotifywrapper.cpp
+++ spotifywrapper.cpp
@@ -14,40 +14,113 @@ SpotifyWrapper::SpotifyWrapper(QObject *parent)
 {
     QUrl authorizationUrl(authorizationBaseUrlString);
     QUrl acessTokenUrl("https://accounts.spotify.com/api/token");
-    oauth2 = std::make_shared<QOAuth2AuthorizationCodeFlow>(clientId,authorizationUrl,acessTokenUrl,nullptr, parent);
+    oauth2 = std::make_shared<QOAuth2AuthorizationCodeFlow>();
+    oauth2->setAuthorizationUrl(authorizationUrl);
+    oauth2->setAccessTokenUrl(acessTokenUrl);
+    oauth2->setClientIdentifier(clientId);
+    oauth2->setClientIdentifierSharedKey(clientSecret);
     replyHandler = new QOAuthHttpServerReplyHandler(port, this);
-    replyHandler->setCallbackPath("callback");
     oauth2->setReplyHandler(replyHandler);
     oauth2->setScope(scope);
 
     connect(&(*oauth2), &QOAuth2AuthorizationCodeFlow::statusChanged, [=](
             QAbstractOAuth::Status status) {
+
         if (status == QAbstractOAuth::Status::Granted)
         {
             qDebug() << "Granted";
             emit authenticated();
         }
-        else{
-            qDebug() << "Not granted";
+        else if (status == QAbstractOAuth::Status::NotAuthenticated){
+            qDebug() << "Not NotAuthenticated";
         }
-    });
-    oauth2->setModifyParametersFunction([&](QAbstractOAuth::Stage stage, QVariantMap *parameters) {
-        if (stage == QAbstractOAuth::Stage::RequestingAuthorization)// && isPermanent())
+
+        else if(status == QAbstractOAuth::Status::TemporaryCredentialsReceived)
         {
-            parameters->insert("duration", "permanent");
-            parameters->insert("client_id",clientId);
-            parameters->insert("redirect_uri",redirectUri);
-            parameters->insert("state","xxxxxxxxxxxxxxx");
+            qDebug() << "TemporaryCredentialsReceived";
+            qDebug()<<oauth2->token();
+
+        }
+        else if(status == QAbstractOAuth::Status::RefreshingToken)
+        {
+            qDebug() << "RefreshingToken";
+
         }
     });
+
+    oauth2->setModifyParametersFunction([&](QAbstractOAuth::Stage stage, QMultiMap<QString, QVariant> *parameters) {
+            if(stage == QAbstractOAuth::Stage::RequestingAuthorization) {
+                qDebug() << * parameters;
+
+                parameters->insert("duration", "permanent");
+                parameters->insert("redirect_uri",redirectUri);
+                parameters->insert("response_type",responseType);
+                parameters->insert("client_id" ,oauth2->clientIdentifier());
+                parameters->insert("client_secret", oauth2->clientIdentifierSharedKey());
+                parameters->insert("scope",scope);
+            }
+            if(stage == QAbstractOAuth::Stage::RequestingAccessToken)
+            {
+                parameters->insert("redirect_uri",redirectUri);
+
+                qDebug() << * parameters;
+                parameters->insert("state",state);
+                parameters->insert("grant_type","authorization_code");
+                parameters->insert("code",code);
+            }
+        });
+
     // open the web desktop browser to perform the authentication
     connect(&(*oauth2), &QOAuth2AuthorizationCodeFlow::authorizeWithBrowser,
             &QDesktopServices::openUrl);
+    connect(&(*oauth2), &QOAuth2AuthorizationCodeFlow::granted,
+                this, &SpotifyWrapper::granted);
+    connect(&(*oauth2), &QOAuth2AuthorizationCodeFlow::error,
+                this, [&](const QString &error, const QString &errorDescription, const QUrl &uri)
+    {
+        qDebug()<<"erro porra\n";
+    });
+    connect(&(*oauth2), &QOAuth2AuthorizationCodeFlow::authorizationCallbackReceived,
+                this, [this](const QVariantMap &data)
+    {
+           code = data["code"].toString();
+           state = data["state"].toString();
+           qDebug() <<"code " << code;
+           qDebug() <<"state "<< oauth2->state();
+           /*emit oauth2->stateChanged(state);
+           auto url = QUrl("https://accounts.spotify.com/api/token");
+           auto reply = oauth2->post(url,data);
+           connect(reply, &QNetworkReply::finished, [=]() {
+                   if (reply->error() != QNetworkReply::NoError) {
+                        qWarning () << "Erro Encontrado";
+                        qWarning() << reply->error();
+                       return;//https://accounts.spotify.com/api/token
+                   }
+
+                   const auto data = reply->readAll();
+
+                   const auto document = QJsonDocument::fromJson(data);
+                   const auto root = document.object();
+
+                   qDebug()<<data;
+                   reply->deleteLater();
+               });*/
+
+
+    });
 
 }
 void SpotifyWrapper::grant()
 {
     oauth2->grant();
-
 }
+void SpotifyWrapper::granted()
+{
+    qDebug()<<"entrou\n";
+}
+QNetworkReply  * SpotifyWrapper::me()
+{
+    return oauth2->get(QUrl("https://api.spotify.com/v1/me"));
+}
+
 
