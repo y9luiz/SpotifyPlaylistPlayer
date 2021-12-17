5b74f2fe15a7c0a775aae2940809f4673dff1753
Luiz <y9luiz@hotmail.com>
2021-12-16 22:27:10 -0300

    added localplaylist.h to header

--- DataAccessObjects/dataaccessobject.h
+++ DataAccessObjects/dataaccessobject.h
@@ -1,5 +1,7 @@
 #ifndef DATAACCESSOBJECT_H
 #define DATAACCESSOBJECT_H
+#include "dataaccessobject.h"
 #include "SpotifyUser/spotifyuser.h"
 #include "SpotifyTrack/spotifytrack.h"
+#include "LocalPlaylist/localplaylist.h"
 #endif // DATAACCESSOBJECT_H
