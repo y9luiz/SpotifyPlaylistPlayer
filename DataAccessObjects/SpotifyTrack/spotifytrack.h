e16f6887730aa0f7ef6931b64fdd0cfb9cab730c
Luiz <y9luiz@hotmail.com>
2021-12-16 21:42:08 -0300

    now inheriting from AbstractDAOObject

--- DataAccessObjects/SpotifyTrack/spotifytrack.h
+++ DataAccessObjects/SpotifyTrack/spotifytrack.h
@@ -1,12 +1,59 @@
 #ifndef SPOTIFYTRACK_H
 #define SPOTIFYTRACK_H
 #include <QString>
-struct SpotifyTrack
+#include "../abstractdaoobject.h"
+class SpotifyTrack : public AbstractDAOObject
 {
-    QString name_;
-    QString artists_;
-    QString id_;
-    QString url_;
+private:
+
+    void setupSpotifyTrack(const QString & name, const QString & artists,const QString & id, const QString & url)
+    {
+        map_["name"] = name;
+        map_["artists"] = artists;
+        map_["id"] = id;
+        map_["url"] = url;
+    }
+    virtual void fromJsonObject(const QJsonObject & jsonObject) override
+    {
+        QString name = jsonObject.value("name").toString();
+        QString id = jsonObject.value("id").toString();
+        QString preview_url = jsonObject.value("preview_url").toString();
+        QString artists;
+
+        const auto jsonArrayArtists = jsonObject.value("artists").toArray();
+
+        foreach (const QJsonValue & value, jsonArrayArtists){
+            const auto artist = value.toObject();
+            artists += artist.value("name").toString() + " ";
+        }
+        setupSpotifyTrack(name,artists,id,preview_url);
+    }
+
+public:
+
+    SpotifyTrack() : SpotifyTrack(QString(""),QString(""),QString(""),QString("")){
+
+    };
+    SpotifyTrack(const QString & name, const QString & artists,const QString & id, const QString & url)
+    {
+        setupSpotifyTrack(name,artists,id,url);
+    };
+    SpotifyTrack(QString && name,QString && artists,QString && id, QString && url)
+    {
+        setupSpotifyTrack(std::move(name),std::move(artists), std::move(id), std::move(url));
+
+    };
+    SpotifyTrack(const QJsonObject & jsonObject)
+    {
+        fromJsonObject(jsonObject);
+    }
+
+
+    inline QString  name() const {return map_["name"].toString();};
+    inline QString url() const {return map_["url"].toString();};
+    inline QString artists() const {return map_["artists"].toString();};
+    inline QString  id() const {return map_["id"].toString();};
+
 };
 
 #endif // SPOTIFYTRACK_H
--- DataAccessObjects/SpotifyUser/spotifyuser.h
+++ DataAccessObjects/SpotifyUser/spotifyuser.h
@@ -2,15 +2,43 @@
 #define SPOTIFYUSER_H
 
 #include <QString>
-struct SpotifyUser
+#include "../abstractdaoobject.h"
+class SpotifyUser : public AbstractDAOObject
 {
-    QString name_;
-    QString email_;
-    QString id_;
-    SpotifyUser(QString && name, QString && email, QString && id):
-        name_(name), email_(email), id_(id)
+private:
+     void setupSpotifyUser(const QString & name, const QString & email,const QString & id)
+     {
+         map_["name"] = name;
+         map_["email"] = email;
+         map_["id"] = id;
+     }
+     virtual void fromJsonObject(const QJsonObject & jsonObject) override
+     {
+         QString name = jsonObject.value("display_name").toString();
+         QString email = jsonObject.value("email").toString();
+         QString id = jsonObject.value("id").toString();
+
+         setupSpotifyUser(name,email,id);
+     }
+
+public:
+
+    SpotifyUser() : SpotifyUser(QString(""),QString(""),QString("")){
+    };
+
+    SpotifyUser(QString && name, QString && email, QString && id)
     {
+       setupSpotifyUser(std::move(name),std::move(email),std::move(id));
     }
+
+    SpotifyUser(const QJsonObject & jsonObject)
+    {
+       fromJsonObject(jsonObject);
+    }
+
+    inline QString name(){return map_["name"].toString();};
+    inline QString email(){return map_["email"].toString();};
+    inline QString id(){return map_["id"].toString();};
 };
 
 #endif // SPOTIFYUSER_H
